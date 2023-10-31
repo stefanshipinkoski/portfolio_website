f8dbab6b83d50e52bd7ca83320dd85c6d7687f00
Stefan Shipinkoski <stefan.shipinkoski@iwconnect.com>
2023-10-31 22:27:47 +0100

    new page design

--- server.py
+++ server.py
@@ -9,9 +9,11 @@ app = Flask(__name__)
 def my_home():
     return render_template('index.html')
 
+
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
 
 def write_to_csv(data):
     with open("/home/stefanshipinkoski/portfolio_website/database.csv", 'a', newline='') as csv_file:
@@ -19,8 +21,10 @@ def write_to_csv(data):
         subject = data["subject"]
         message = data["message"]
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
-        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer = csv.writer(csv_file, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email, subject, message, timestamp])
+
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
@@ -34,6 +38,7 @@ def submit_form():
 
 @app.route('/robots.txt')
 def noindex():
-    r = Response(response="User-Agent: *\nDisallow: \n", status=200, mimetype="text/plain")
+    r = Response(response="User-Agent: *\nDisallow: \n",
+                 status=200, mimetype="text/plain")
     r.headers["Content-Type"] = "text/plain; charset=utf-8"
     return r
